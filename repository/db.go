1907896e83f4d0d6f31702b2ee8b108ce8c0e5bc
atareversei (pc) <faraji.ata92@gmail.com>
2023-11-30 12:13:33 +0330

    add postgres driver

--- repository/db.go
+++ repository/db.go
@@ -1,11 +1,35 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"os"
+)
 
-type DB struct {
-	db *sql.DB
+type Config struct {
+	Username string
+	Password string
+	Port     int
+	Host     string
+	DBName   string
 }
 
-func New() {
+type DB struct {
+	config Config
+	db     *pgxpool.Pool
+}
 
+func New(config Config) *DB {
+	db, err := pgxpool.New(context.Background(), fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", config.Username, config.Password, config.Host, config.Port, config.DBName))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	return &DB{
+		config: config,
+		db:     db,
+	}
 }
